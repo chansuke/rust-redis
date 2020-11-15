8491c8d47597130d83f5535a88800f342814bd4c
chansuke <chansuke@georepublic.de>
2020-11-15 20:53:46 +0900

    init

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use mini_redis::{client, Result};
+
+#[tokio::main]
+pub async fn main() -> Result<()> {
+    let mut client = client::connect("127.0.0.1:3001").await?;
+
+    client.set("hello", "world".into()).await?;
+
+    let result = client.get("hello").await?;
+
+    println!("result is {:?}", result);
+
+    Ok(())
+}
